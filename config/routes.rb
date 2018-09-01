81c518107b434a95f29c569a019958eb0450fec4
Diego souza <diegosouzafl94@gmail.com>
2018-09-01 10:30:54 -0300

    formulario new session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  #sessions users
+  get 'sessions/new'
+  get    'sign_in'   => 'sessions#new'
+  post   'sign_in'   => 'sessions#create'
+  delete 'sign_out'  => 'sessions#destroy'
+
+
   get 'users/new'
   get 'static_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
